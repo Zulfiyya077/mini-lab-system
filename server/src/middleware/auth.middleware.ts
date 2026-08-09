a8ef29ab1fd259a76fddca4ead1dc7588ae740de
Zulfiyya Mammadli <mammadli.zulfiyya07@gmail.com>
2026-08-09 21:28:20 +0400

    feat: implement authentication with JWT

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,30 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type Jwt } from "jsonwebtoken";
+import { AppError } from "../errors/AppError.js";
+import type { JwtPayload } from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in the environment variables.");
+}   
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.accessToken;  
+    console.log("Access token from cookie:", token);
+
+    if (!token) {
+        return res.status(401).json({ error: "Access token is missing", success: false, statusCode: 401 });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+        req.user = decoded;
+     console.log("Decoded user from token:", req.cookies);
+
+        next();
+    } catch (err) {
+        console.error("Token verification failed:", err);
+       throw new AppError("Invalid access token", 401);
+    }
+};
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../lib/prisma.js";  
 import { AppError } from "../errors/AppError.js";
+import { generateAccessToken } from "../utils/jwt.js";
 
 
 
@@ -27,3 +28,29 @@ export const registerUser = async (name: string,  email: string, password: strin
     };
 }   
     
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+        throw new AppError("Invalid email or password", 401);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        throw new AppError("Invalid email or password", 401);
+    }
+
+  const accessToken = generateAccessToken(user.id, user.role);
+
+    return {
+       
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+        accessToken
+    };
+}
